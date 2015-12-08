630b16e8b1bd913cb7dab6e2869035a39c0d7a3c
rkmaduka <rkmaduka@quinnipiac.edu>
2015-12-08 14:55:35 +0000

    Add user delete view

--- core/views.py
+++ core/views.py
@@ -126,33 +126,51 @@ class VoteFormView(FormView):
                 prev_votes[0].delete()
         return redirect('review_list')
       
-      class UserDetailView(DetailView):
-          model = User
-          slug_field = 'username'
-          template_name = 'user/user_detail.html'
-          context_object_name = 'user_in_view'
+class UserDetailView(DetailView):
+    model = User
+    slug_field = 'username'
+    template_name = 'user/user_detail.html'
+    context_object_name = 'user_in_view'
           
-          def get_context_data(self, **kwargs):
-              context = super(UserDetailView, self).get_context_data(**kwargs)
-              user_in_view = User.objects.get(username=self.kwargs['slug']
-              reviews = Review.objects.filter(user=user_in_view)
-              context['reviews'] = reviews
-              comments = Comments.objects.filter(user=user_in_view)
-              context['comments'] = comments
-              return context
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user_in_view = User.objects.get(username=self.kwargs['slug']
+        reviews = Review.objects.filter(user=user_in_view)
+        context['reviews'] = reviews
+        comments = Comments.objects.filter(user=user_in_view)
+        context['comments'] = comments
+        return context
               
-        class UserUpdateView(UpdateView):
-            model = User
-            slug_field = "username"
-            template_name = "user/user_form.html"
-            fields = ['email', 'first_name', 'last_name']
+class UserUpdateView(UpdateView):
+    model = User
+    slug_field = "username"
+    template_name = "user/user_form.html"
+    fields = ['email', 'first_name', 'last_name']
             
-            def get_success_url(self):
-                return reverse('user_detail ', args=[self.request.user.username]
+    def get_success_url(self):
+        return reverse('user_detail ', args=[self.request.user.username]
                 
-            def get_object(self, *args, **kwargs):
-                object = super(UserUpdateView, self).get_object(*args, **kwargs)
-                if object != self.request.user:
-                    raise PermissionDenied
-                return object 
+    def get_object(self, *args, **kwargs):
+        object = super(UserUpdateView, self).get_object(*args, **kwargs)
+        if object != self.request.user:
+            raise PermissionDenied()
+        return object 
                 
+class UserDeleteView(DeleteView):
+    model = User
+    slug_field = "username"
+    template_name = 'user/user_confirm_delete.html'
+    
+    def get_success_url(self, *args, **kwargs):
+        object = super(UserDeleteView, self).get_object(*args, **kwargs)
+        if object != self.request.user:
+            raise PermissionDenied()
+        return object
+        
+    def delete(self, request, *args, **kwargs):
+        user = super(UserDeleteView, self).get_object(*args)
+        user.is_active = False
+        user.save()
+        return redirect(self.get_success_url())
+        
+        
